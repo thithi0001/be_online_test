72d139be7325255b9c442961791c47ae1d671d44
thithi0001 <ledinhthi0002@gmail.com>
2026-06-07 18:14:52 +0700

    repair for mail module

---
+++ src/modules/email/email.module.ts
@@ -0,0 +1,19 @@
+// import { Module } from "@nestjs/common";
+// import { MailerModule } from "@nestjs-modules/mailer";
+
+// @Module({
+//   imports: [
+//     MailerModule.forRoot({
+//       transport: {
+//         host: 'smtp.gmail.com',
+//         port: 587,
+//         secure: false,
+//         auth: {
+//           user: process.env.MAIL_USER,
+//           pass: process.env.MAIL_PASSWORD,
+//         },
+//       },
+//     }),
+//   ],
+// })
+// export class EmailModule {}
---
+++ src/modules/email/email.service.ts
@@ -0,0 +1,21 @@
+// import { Injectable } from "@nestjs/common";
+// import { MailerService } from "@nestjs-modules/mailer";
+
+// @Injectable()
+// export class EmailService {
+//   constructor(
+//     private readonly mailerService: MailerService,
+//   ) {}
+
+//   async sendMail(
+//     to: string,
+//     subject: string,
+//     html: string,
+//   ) {
+//     await this.mailerService.sendMail({
+//       to,
+//       subject,
+//       html,
+//     });
+//   }
+// }
